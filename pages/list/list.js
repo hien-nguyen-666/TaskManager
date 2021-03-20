c4f27c9c9f01be3868398c3e2b5273fb58c3bff0
hung_nguyen <hung_nguyen@vision-net.co.jp>
2021-03-20 20:12:35 +0700

    20:11 Thêm JS trên file list và sửa lại layout

---
+++ pages/list/list.js
@@ -0,0 +1,16 @@
+// function openForm1(params) {
+//   document.getElementById("myButton1").style.display = "block";
+// }
+
+// Mở popup
+openForm1 = () =>
+  (document.getElementById("myPopup1").style.display = "block");
+openForm2 = () =>
+  (document.getElementById("myPopup2").style.display = "block");
+openForm3 = () =>
+  (document.getElementById("myPopup3").style.display = "block");
+openForm4 = () =>
+  (document.getElementById("myPopup4").style.display = "block");
+
+closeForm = () =>
+  (document.getElementById("myPopup1").style.display = "none");

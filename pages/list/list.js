04627c0424a09cd277ac6a11440de81602c57f98
hung_nguyen <hung_nguyen@vision-net.co.jp>
2021-03-22 12:12:44 +0700

    12:12 Thêm JavaScript

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,16 +1,36 @@
-// function openForm1(params) {
-//   document.getElementById("myButton1").style.display = "block";
-// }
+// openForm1 = () = ()
+// Hình như chỉ cho phép 1 dòng code
 
 // Mở popup
-openForm1 = () =>
-  (document.getElementById("myPopup1").style.display = "block");
-openForm2 = () =>
-  (document.getElementById("myPopup2").style.display = "block");
-openForm3 = () =>
-  (document.getElementById("myPopup3").style.display = "block");
-openForm4 = () =>
-  (document.getElementById("myPopup4").style.display = "block");
+function openForm1() {
+  document.getElementById("myPopup1").style.display = "block";
+  document.getElementById("myPopup2").style.display = "none";
+  document.getElementById("myPopup3").style.display = "none";
+  document.getElementById("myPopup4").style.display = "none";
+}
+function openForm2() {
+  document.getElementById("myPopup1").style.display = "none";
+  document.getElementById("myPopup2").style.display = "block";
+  document.getElementById("myPopup3").style.display = "none";
+  document.getElementById("myPopup4").style.display = "none";
+}
+function openForm3() {
+  document.getElementById("myPopup1").style.display = "none";
+  document.getElementById("myPopup2").style.display = "none";
+  document.getElementById("myPopup3").style.display = "block";
+  document.getElementById("myPopup4").style.display = "none";
+}
+function openForm4() {
+  document.getElementById("myPopup1").style.display = "none";
+  document.getElementById("myPopup2").style.display = "none";
+  document.getElementById("myPopup3").style.display = "none";
+  document.getElementById("myPopup4").style.display = "block";
+}
 
-closeForm = () =>
-  (document.getElementById("myPopup1").style.display = "none");
+// Đóng popup
+function closeForm() {
+  document.getElementById("myPopup1").style.display = "none";
+  document.getElementById("myPopup2").style.display = "none";
+  document.getElementById("myPopup3").style.display = "none";
+  document.getElementById("myPopup4").style.display = "none";
+}
